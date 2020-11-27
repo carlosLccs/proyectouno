189ee7660990758f6a78c22b87f9e9f1a42d66da
carlosLccs <70754848+carlosLccs@users.noreply.github.com>
2020-11-27 19:24:58 -0400

    Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -26,9 +26,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '#g-=$eezqif@_uib7f^4evg@t5g4k^u&w&x4#sk-cj@*u(tutf'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [*]
 
 
 # Application definition
